4c553c2d527ddf1226cb8144691047c5b1b9dc24
Gerald Scharitzer <no@e.mail>
2022-10-30 11:28:11 +0100

    list backends

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from getpass import getpass
+
 import qiskit
 from qiskit_aer import AerSimulator
+from qiskit import IBMQ
+
+token = getpass("token: ")
+provider = IBMQ.enable_account(token)
+print("provider", provider)
+backends = provider.backends()
+for backend in backends:
+    print("backend", backend)
 
 circuit = qiskit.QuantumCircuit(2)
 circuit.h(0)
